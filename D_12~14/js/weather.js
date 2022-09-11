bfda35fb4779d2d67b5a2f7cc1774d26623e5c3d
Heen <agny_@naver.com>
2022-09-12 01:37:09 +0900

    :memo: completed! + edit

--- D_12~14/js/weather.js
+++ D_12~14/js/weather.js
@@ -1,11 +1,13 @@
+import config from "./apikey";
+
 const city = document.querySelector("#weather span:first-child");
 const weather = document.querySelector("#weather span:last-child");
-const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
+const WEATHER_API = config.apikey;
 
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=5321d631cf63466b3d7ece82b62232a2&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API}&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
